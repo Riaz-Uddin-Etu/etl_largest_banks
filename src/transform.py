8ad78d26c4cd9723faed079e0b2808b9aa75a54f
Riaz-Uddin-Etu <riazuddinetu23@gmail.com>
2026-01-03 19:20:13 +0600

    refactor: updated BDT exchange rate

--- src/transform.py
+++ src/transform.py
@@ -6,10 +6,10 @@ def transform(df, csv_path):
     exchange_rate_df = pd.read_csv(csv_path, index_col='Currency')
     euro_rate = exchange_rate_df.loc['EUR','Rate']
     gbp_rate = exchange_rate_df.loc['GBP','Rate']
-    inr_rate = exchange_rate_df.loc['INR','Rate']
+    inr_rate = exchange_rate_df.loc['BDT','Rate']
     dataframe_euro = (df[['MC_USD_Billion']] * euro_rate).rename(columns={'MC_USD_Billion':'MC_EURO_Billion'})
     dataframe_gbp = pd.DataFrame({'MC_GBP_Billion': df['MC_USD_Billion'] * gbp_rate})
-    dataframe_inr = (df[['MC_USD_Billion']] * inr_rate).rename(columns={'MC_USD_Billion': 'MC_INR_Billion'})
+    dataframe_inr = (df[['MC_USD_Billion']] * inr_rate).rename(columns={'MC_USD_Billion': 'MC_BDT_Billion'})
     
     dataframe2 = pd.concat([df, dataframe_euro, dataframe_gbp, dataframe_inr], axis=1).round(2)
     return dataframe2
